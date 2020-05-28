002e51a77f03d7885bbe676e46008b0b9595c304
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2020-05-28 09:40:10 -0300

    Fix PHP 5.3 compatibility

--- tests/Unit/Exception/ContainerExceptionTest.php
+++ tests/Unit/Exception/ContainerExceptionTest.php
@@ -1,10 +1,20 @@
 <?php
 
+/**
+ * This file is part of coisa/container.
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ * @link      https://github.com/coisa/container
+ * @copyright Copyright (c) 2019-2020 Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
 namespace CoiSA\Container\Test\Unit\Exception;
 
 use CoiSA\Container\Exception\ContainerException;
 use PHPUnit\Framework\TestCase;
-use Psr\Container\ContainerExceptionInterface;
 
 /**
  * Class ContainerExceptionTest
@@ -15,11 +25,11 @@ final class ContainerExceptionTest extends TestCase
 {
     public function testCreateFromExceptionForIdentifierWillReturnContainerExceptionWithGivenPreviousException()
     {
-        $exception = new \Exception(\uniqid('exception', true), \random_int(1, 1000));
+        $exception          = new \Exception(\uniqid('exception', true), \mt_rand(1, 1000));
         $containerException = ContainerException::createFromExceptionForIdentifier($exception, \uniqid('id', true));
 
-        $this->assertInstanceOf(ContainerException::class, $containerException);
-        $this->assertInstanceOf(ContainerExceptionInterface::class, $containerException);
+        $this->assertInstanceOf('CoiSA\Container\Exception\ContainerException', $containerException);
+        $this->assertInstanceOf('Psr\Container\ContainerExceptionInterface', $containerException);
         $this->assertSame($exception, $containerException->getPrevious());
     }
 }
--- tests/Unit/Exception/NotFoundExceptionTest.php
+++ tests/Unit/Exception/NotFoundExceptionTest.php
@@ -1,10 +1,20 @@
 <?php
 
+/**
+ * This file is part of coisa/container.
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ * @link      https://github.com/coisa/container
+ * @copyright Copyright (c) 2019-2020 Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
 namespace CoiSA\Container\Test\Unit\Exception;
 
 use CoiSA\Container\Exception\NotFoundException;
 use PHPUnit\Framework\TestCase;
-use Psr\Container\NotFoundExceptionInterface;
 
 /**
  * Class NotFoundExceptionTest
@@ -17,7 +27,7 @@ final class NotFoundExceptionTest extends TestCase
     {
         $notFoundException = NotFoundException::createForIdentifier(\uniqid('id', true));
 
-        $this->assertInstanceOf(NotFoundException::class, $notFoundException);
-        $this->assertInstanceOf(NotFoundExceptionInterface::class, $notFoundException);
+        $this->assertInstanceOf('CoiSA\Container\Exception\NotFoundException', $notFoundException);
+        $this->assertInstanceOf('Psr\Container\NotFoundExceptionInterface', $notFoundException);
     }
 }
