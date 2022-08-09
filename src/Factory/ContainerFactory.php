0c99f2b6b49aea9058f59701534ea5764a01a995
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2022-08-08 21:26:25 -0300

    Refactoring

--- src/Factory/ContainerFactory.php
+++ src/Factory/ContainerFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * This file is part of coisa/container.
  *
@@ -7,19 +9,21 @@
  * with this source code in the file LICENSE.
  *
  * @link      https://github.com/coisa/container
- *
- * @copyright Copyright (c) 2019-2020 Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
+ * @copyright Copyright (c) 2019-2022 Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
  * @license   https://opensource.org/licenses/MIT MIT License
  */
+
 namespace CoiSA\Container\Factory;
 
 use CoiSA\Container\Container;
+use CoiSA\Container\ContainerInterface;
 use CoiSA\Factory\AbstractFactory;
 use CoiSA\Factory\Exception\InvalidArgumentException;
 use CoiSA\ServiceProvider\AggregateServiceProvider;
+use CoiSA\ServiceProvider\Exception\ReflectionException;
 use CoiSA\ServiceProvider\LaminasConfigServiceProvider;
 use Interop\Container\ServiceProviderInterface;
-use Psr\Container\ContainerInterface;
+use Psr\Container\ContainerInterface as PsrContainerInterface;
 
 /**
  * Class ContainerFactory.
@@ -33,10 +37,8 @@ final class ContainerFactory implements ContainerFactoryInterface
      */
     private $aggregateServiceProvider;
 
-    /**,
+    /**
      * ContainerFactory constructor.
-     *
-     * @param AggregateServiceProvider $aggregateServiceProvider
      */
     public function __construct(AggregateServiceProvider $aggregateServiceProvider)
     {
@@ -44,41 +46,72 @@ final class ContainerFactory implements ContainerFactoryInterface
     }
 
     /**
-     * @throws \CoiSA\Factory\Exception\ReflectionException
-     * @throws \CoiSA\Factory\Exception\InvalidArgumentException
-     *
-     * @return ContainerInterface
+     * @throws InvalidArgumentException
+     * @throws ReflectionException
      */
-    public function create()
+    public function create(): ContainerInterface
     {
         $serviceProviders = \func_get_args();
         $container        = new Container($this->aggregateServiceProvider);
 
-        foreach ($serviceProviders as $serviceProvider) {
-            if (\is_string($serviceProvider)) {
-                $serviceProvider = AbstractFactory::create($serviceProvider);
-            }
+        $this->registerServiceProviders($serviceProviders, $container);
 
-            if (\is_callable($serviceProvider)) {
-                $serviceProvider = \call_user_func($serviceProvider);
-            }
+        $container->setService(ContainerInterface::class, $container);
+        $container->setAlias(PsrContainerInterface::class, ContainerInterface::class);
 
-            if (\is_array($serviceProvider)) {
-                $serviceProvider = new LaminasConfigServiceProvider($serviceProvider);
-            }
-
-            if (!$serviceProvider instanceof ServiceProviderInterface) {
-                throw InvalidArgumentException::forInvalidArgumentType(
-                    'serviceProviders',
-                    'array<Interop\\Container\\ServiceProviderInterface|callable|array|string>'
-                );
-            }
-
-            $container->register($serviceProvider);
+        if (class_exists(AbstractFactory::class)) {
+            AbstractFactory::setContainer($container);
         }
 
-        AbstractFactory::setContainer($container);
-
         return $container;
+    }
+
+    /**
+     * @param array<array<string, array>|callable|ServiceProviderInterface|string> $serviceProviders
+     */
+    private function registerServiceProviders(array $serviceProviders, Container $container): void
+    {
+        try {
+            foreach ($serviceProviders as $serviceProvider) {
+                $serviceProvider = $this->resolveServiceProvider($serviceProvider, $container);
+                $container->register($serviceProvider);
+            }
+        } catch (\Throwable $throwable) {
+            throw InvalidArgumentException::forInvalidArgumentType(
+                'serviceProviders',
+                'array<string|callable|' . ServiceProviderInterface::class . '|array<string, array>>',
+                0,
+                $throwable
+            );
+        }
+    }
+
+    /**
+     * @param array<string,array>|callable|ServiceProviderInterface|string $serviceProvider
+     *
+     * @throws ReflectionException
+     */
+    private function resolveServiceProvider($serviceProvider, Container $container): ServiceProviderInterface
+    {
+        if (\is_string($serviceProvider)) {
+            $serviceProvider = AbstractFactory::create($serviceProvider);
+        }
+
+        if (\is_callable($serviceProvider)) {
+            $serviceProvider = \call_user_func($serviceProvider, $container);
+        }
+
+        if (\is_array($serviceProvider)) {
+            $serviceProvider = new LaminasConfigServiceProvider($serviceProvider);
+        }
+
+        if (!$serviceProvider instanceof ServiceProviderInterface) {
+            throw InvalidArgumentException::forInvalidArgumentType(
+                'serviceProvider',
+                'string|callable|' . ServiceProviderInterface::class . '|array<string,array>'
+            );
+        }
+
+        return $serviceProvider;
     }
 }
